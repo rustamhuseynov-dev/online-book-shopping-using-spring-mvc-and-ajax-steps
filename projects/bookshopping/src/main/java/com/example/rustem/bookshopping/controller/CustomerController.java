0c8644bdb83d4168f987daee4167ad7bc8bd4af0
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-03-24 14:26:16 +0400

    Alıcı səhifəsinin dizayn olunması

--- projects/bookshopping/src/main/java/com/example/rustem/bookshopping/controller/CustomerController.java
+++ projects/bookshopping/src/main/java/com/example/rustem/bookshopping/controller/CustomerController.java
@@ -1,6 +1,10 @@
 package com.example.rustem.bookshopping.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,7 +16,12 @@ import lombok.RequiredArgsConstructor;
 public class CustomerController {
 
 	@GetMapping
-	public String showCustomerPage() {
+	public String showCustomerPage(Model model) {
+		List<String> books = new ArrayList<>();
+		for (int i = 1; i < 100; i++) {
+			books.add("");
+		}
+		model.addAttribute("books", books);
 		return "customer";
 	}
 }
