ea01fed5f85503f4e82f049137de4af05ed8ab2e
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-03-27 14:52:56 +0400

    Update StorageServiceImpl.java

--- projects/bookshopping/src/main/java/com/example/rustem/bookshopping/service/impl/StorageServiceImpl.java
+++ projects/bookshopping/src/main/java/com/example/rustem/bookshopping/service/impl/StorageServiceImpl.java
@@ -1,7 +1,44 @@
 package com.example.rustem.bookshopping.service.impl;
 
+import java.nio.file.Path;
+
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.example.rustem.bookshopping.service.StorageService;
 
+@Service
 public class StorageServiceImpl implements StorageService {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String store(MultipartFile multipartFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Path load(String filename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Resource loadAsResource(String filename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
