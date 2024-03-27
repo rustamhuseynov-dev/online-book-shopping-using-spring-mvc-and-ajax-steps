63f671a2fe0bf6428e041363c3ee3a85e1fa21a8
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-03-27 15:39:58 +0400

    Faylın endirmə kodları

--- projects/bookshopping/src/main/java/com/example/rustem/bookshopping/service/impl/StorageServiceImpl.java
+++ projects/bookshopping/src/main/java/com/example/rustem/bookshopping/service/impl/StorageServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.rustem.bookshopping.service.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,11 +10,13 @@ import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.rustem.bookshopping.exception.StorageException;
+import com.example.rustem.bookshopping.exception.StorageFileNotFoundException;
 import com.example.rustem.bookshopping.properties.StorageProperties;
 import com.example.rustem.bookshopping.service.StorageService;
 
@@ -59,14 +62,22 @@ public class StorageServiceImpl implements StorageService {
 
 	@Override
 	public Path load(String filename) {
-		// TODO Auto-generated method stub
-		return null;
+		return rootLocation.resolve(filename);
 	}
 
 	@Override
 	public Resource loadAsResource(String filename) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Path file = load(filename);
+			Resource resource = new UrlResource(file.toUri());
+			if (resource.exists() || resource.isReadable()) {
+				return resource;
+			} else {
+				throw new StorageFileNotFoundException("Fayl oxuna bilmedi: " + filename);
+			}
+		} catch (MalformedURLException e) {
+			throw new StorageFileNotFoundException("Fayl oxuna bilmedi: " + filename, e);
+		}
 	}
 
 	@Override
